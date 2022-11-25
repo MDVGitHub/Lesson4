cf7b00188a8c62d947f4f3ec559e99f5d1707dfe
Dmitriy Makarov <d.makarov.mail@gmail.com>
2022-11-25 18:46:21 +0300

    dz4_1

---
+++ dz4_1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+Console.WriteLine("Введите число А");
+int A = Console.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int B = Console.ToInt32(Console.ReadLine());
+
+int Exponentiation ()
+{
+    for (int i = 0; i < B; i++)
+    {
+        int count = 0;
+        int stek = count * A;
+        return count;
+    }
+}
+Console.WriteLine($"Результат возведения {A} в степень {B} равен {count}");
